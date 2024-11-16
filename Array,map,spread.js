d658857237ddeb3111380724d98330d562cad296
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-16 12:43:38 +0530

    slice & spread operator to copy

--- Array,map,spread.js
+++ Array,map,spread.js
@@ -1,4 +1,7 @@
-array=['apple','orange','','mango','','lemon'];
+const array=['apple','orange','','mango','','lemon'];
 newArray=array.map((fruit)=>fruit===''?"empty string": fruit  
 )
-console.log(newArray);
+copiedArray=array.slice();//slice() to copy
+console.log(copiedArray);
+copied2Array=[...array];//spread operator to copy
+console.log(copied2Array);
