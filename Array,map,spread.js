7289ebfd5d34613af23d71803266e6b49b7ff1e3
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-16 13:02:02 +0530

    learning

--- Array,map,spread.js
+++ Array,map,spread.js
@@ -1,7 +1,38 @@
-const array=['apple','orange','','mango','','lemon'];
-newArray=array.map((fruit)=>fruit===''?"empty string": fruit  
-)
-copiedArray=array.slice();//slice() to copy
-console.log(copiedArray);
-copied2Array=[...array];//spread operator to copy
-console.log(copied2Array);
+// const array=['apple','orange','','mango','','lemon'];
+// const newArray=array.map((fruit)=>fruit===''?"empty string": fruit  
+// )
+// const copiedArray=array.slice();//slice() to copy
+// console.log(copiedArray);
+// const copied2Array=[...array];//spread operator to copy
+// console.log(copied2Array);
+// const arr=(...args)=>{
+//     return args;
+// }
+// console.log(arr(1,2,3,4));//rest operator
+// const obj1 = {'key1': 1}
+
+// const obj2 = { ...obj1}
+
+// // if(obj2 === obj1){
+
+// // console.log('same objects')
+
+// // }
+
+// // else{
+
+// // console.log('different objects')
+
+// // }
+// console.log(obj1);
+// console.log(obj2);
+
+const obj1 = {'key1': 1 , 'key2' : 2}
+
+const obj2 = { ...obj1, key1: 1000}
+
+
+
+console.log(obj1)
+
+console.log(obj2)
