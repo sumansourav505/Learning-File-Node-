b146f6558fdfc8fdceecf463b23b3357e6886969
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-18 00:02:07 +0530

    Deliverable write & read from file

--- CreateNode/server.js
+++ CreateNode/server.js
@@ -1,15 +1,15 @@
-const http=require('http');
+const http=require('http');//import the http module
 // function rqListener(req,res){
 
 // }
 // http.createServer(function(req,res){
     
 // });
-const server=http.createServer((req,res)=>{
+const server=http.createServer((req,res)=>{// create server & perform response operation to client according to request
     res.setHeader('Content-Type','text/plain');
     const url=req.url;
     if(url==='/home'){
-        res.write('Welcome home');
+        res.write('Welcome home page');
     }
     if(url==='/about'){
         res.write('Welcome to About Us page');
@@ -18,11 +18,5 @@ const server=http.createServer((req,res)=>{
         res.write('Welcome my Node js project');
     }
     res.end();
-    // res.setHeader('Content-Type','text/html');
-    // res.write('<html>');
-    // res.write('<head><title>My First Page</title></head>');
-    // res.write('<body><h1>Wellcome to my Node js project</h1></body>');
-    // res.write('</html>');
-    // res.end();
 });
 server.listen(4000);
