3ec80143a75a47191689b22b0a77aa446f2d952b
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-17 12:02:15 +0530

    request & response task

--- CreateNode/server.js
+++ CreateNode/server.js
@@ -6,8 +6,23 @@ const http=require('http');
     
 // });
 const server=http.createServer((req,res)=>{
-    console.log("Suman Sourav Barik");
+    res.setHeader('Content-Type','text/plain');
+    const url=req.url;
+    if(url==='/home'){
+        res.write('Welcome home');
+    }
+    if(url==='/about'){
+        res.write('Welcome to About Us page');
+    }
+    if(url==='/node'){
+        res.write('Welcome my Node js project');
+    }
+    res.end();
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>My First Page</title></head>');
+    // res.write('<body><h1>Wellcome to my Node js project</h1></body>');
+    // res.write('</html>');
+    // res.end();
 });
-server.listen(4000,()=>{
-    console.log("server running on http://localhost:4000")
-});
+server.listen(4000);
