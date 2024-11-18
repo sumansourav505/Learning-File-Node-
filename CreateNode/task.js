bd0866936ba978314b2fdf9104bab046e111a4ef
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-18 17:56:25 +0530

    modifications

--- CreateNode/task.js
+++ CreateNode/task.js
@@ -1,36 +1,7 @@
 const http=require('http');
-const fs=require('fs');
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url==='/'){
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url==='/message' && method==="POST"){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message,err=>{
-                req.statusCode=302;
-                res.setHeader('Location','/');
-                return res.end();
-            });
-        });
-    }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>Enter message</title></head>');
-    res.write('<body><h1>Wellcome to my Node js project</h1></body>');
-    res.write('</html>');
-    res.end();
+const routes=require('./routes');
+const server=http.createServer(routes.handler);
+console.log(routes.someText);
+server.listen(4000,()=>{
+    console.log(`server is running locallhost ${4000}`);
 });
-server.listen(4000);
