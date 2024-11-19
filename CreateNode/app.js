458249d9e3873371d6fb77e505a5a6a72f962e91
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-19 23:19:02 +0530

    express makes easier

--- CreateNode/app.js
+++ CreateNode/app.js
@@ -1,7 +1,18 @@
-const http=require('http');
-const routes=require('./routes');
-const server=http.createServer(routes.handler);
-console.log(routes.someText);
-server.listen(4000,()=>{
-    console.log(`server is running locallhost ${4000}`);
+// const http=require('http');
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log("middle ware");
+    next();//allow to continue to execute another middleware
+})
+app.use((req,res,next)=>{
+    console.log("Another middle ware");
+    res.send("<h1>Hello from Express</h1>")
+})
+// const server=http.createServer(app);
+// server.listen(4000,()=>{
+//     console.log(`server is running locallhost ${4000}`);
+// });
+app.listen(4000,()=>{
+    console.log("server running on 4000");
 });
