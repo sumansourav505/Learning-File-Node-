ab1f4245b3e94aa39b7bc2c04b23ee9f22e41165
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-20 12:05:28 +0530

    Add body parser and handle the routing

--- CreateNode/app.js
+++ CreateNode/app.js
@@ -1,18 +1,20 @@
 // const http=require('http');
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("middle ware");
-    next();//allow to continue to execute another middleware
-})
-app.use((req,res,next)=>{
-    console.log("Another middle ware");
-    res.send("<h1>Hello from Express</h1>")
-})
-// const server=http.createServer(app);
-// server.listen(4000,()=>{
-//     console.log(`server is running locallhost ${4000}`);
-// });
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>")
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+
+});
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Hello from express</h1>")
+});
+
 app.listen(4000,()=>{
     console.log("server running on 4000");
 });
