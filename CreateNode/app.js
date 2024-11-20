66ba2e447c7517af4d4a478baad9dd07252776bc
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-20 23:50:22 +0530

    routes,404 error and filter

--- CreateNode/app.js
+++ CreateNode/app.js
@@ -1,20 +1,17 @@
 // const http=require('http');
 const express=require('express');
 const bodyParser=require('body-parser');
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>")
-});
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
 
-});
-app.use('/',(req,res,next)=>{
-    res.send("<h1>Hello from express</h1>")
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 app.listen(4000,()=>{
     console.log("server running on 4000");
 });
