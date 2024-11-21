8b2fa71215b0e1126df7991faa41d695f1d5df1f
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-21 22:29:25 +0530

    adding external css and use express.static to link the css

--- CreateNode/app.js
+++ CreateNode/app.js
@@ -7,7 +7,9 @@ const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
 
 const app=express();
+
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
