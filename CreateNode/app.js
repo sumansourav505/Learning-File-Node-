e29b1685ee4bf9b75388708f37c91eb7f3868b20
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-11-29 00:33:19 +0530

    Fetching products

--- CreateNode/app.js
+++ CreateNode/app.js
@@ -14,11 +14,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT*FROM products').then(res=>{
-    console.log(res[0],res[1]);
-}).catch(err=>{
-    console.log(err);
-});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
