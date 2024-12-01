e801f1355841ec1a13d0854264c1ed31f116d983
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-12-02 00:17:45 +0530

    creat cart and cartlist table in db

--- CreateNode/app.js
+++ CreateNode/app.js
@@ -1,13 +1,13 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize=require('./util/database');
-const Product=require('./models/product');
-const User=require('./models/user');
-
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item'); // Ensure this is correctly imported
 
 const app = express();
 
@@ -17,43 +17,61 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
-    User.findByPk(1).then(user=>{
-        req.user=user;
+// Middleware to attach a user to every request
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      if (user) {
+        req.user = user;
         next();
-    }).catch(err=>console.log(err));
+      } else {
+        // Handle case where user doesn't exist
+        return User.create({ name: 'Suman', email: 'test@gmail.com' }).then(newUser => {
+          req.user = newUser;
+          next();
+        });
+      }
+    })
+    .catch(err => {
+      console.log('Error finding user:', err);
+      next(); // Proceed even if there's an error (optional)
+    });
 });
 
+// Route handling
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
-Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+// Define model relationships
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
-sequelize.// sync({force:true})
-    sync()
-    .then(res=>{
-        return User.findByPk(1);
-    // console.log(res);
-}).then(user=>{
-    if(!user){
-        User.create({name:'Suman',email:'test@gmail.com'});
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+// Sync database and start the server
+sequelize.sync({force:true}) // Removed { force: true } for data persistence
+  .then(() => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      // Return the user creation to ensure it's available for the next `.then`
+      return User.create({ name: 'Suman', email: 'test@gmail.com' });
     }
     return user;
-})
-.then(user=>{
-    // console.log(user);
-    app.listen(3000,()=>{
-        console.log(`server running on http://localhost:3000/`)
+  })
+  .then(user => {
+    console.log('User initialized:', user.name); // Optional logging
+    app.listen(3000, () => {
+      console.log('Server running on http://localhost:3000/');
     });
-})
-.catch(err=>{
-    console.log(err);
-})
-
-
+  })
+  .catch(err => {
+    console.error('Error initializing the application:', err);
+  });
--- CreateNode/controllers/shop.js
+++ CreateNode/controllers/shop.js
@@ -46,7 +46,7 @@ exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
     Product.fetchAll(products => {
       const cartProducts = [];
-      for (product of products) {
+      for (const product of products) {
         const cartProductData = cart.products.find(
           prod => prod.id === product.id
         );
